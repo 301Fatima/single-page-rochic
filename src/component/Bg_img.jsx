bd4068258d05b7936bfacb083310db447ffbc591
301Fatima <132878418+301Fatima@users.noreply.github.com>
2023-12-28 19:04:17 +0500

    Update Bg_img.jsx

--- src/component/Bg_img.jsx
+++ src/component/Bg_img.jsx
@@ -4,25 +4,25 @@ import Background from '../assets/Background.jpg'
 const Bg_img = () => {
   return (
     <div>
-       <div className='relative'>
-                <img src={Background} alt='img' className='bg-fixed bg-no-repeat bg-cover mt-[6.4rem] opacity-95 blur-[1px]' />
-
-                <div className='h-[10rem] md:w-[30rem] sm:w-full absolute z-10 lg:mt-[-35rem] lg:ml-[10rem] md:ml-[10rem] md:mt-[-23rem] sm:ml-6 sm:mt-[-18rem] '>
-                    <h1 className='lg:text-5xl md:text-4xl sm:text-lg font-semibold leading-tight text-white'>
-                        The full-service <br />interior design <br />studio
-                    </h1>
-                    <br></br>
-                    <p className='text-lg leading-5 text-white font-thin'>
-                        Leading the digital transformation of railway With decades of maintenance
+        <div className='relative'>
+            <div style={{ backgroundImage:`url(${Background})`}} className='bg-local bg-no-repeat bg-cover mt-[6.4rem] h-[800px]' >
+            <section className="text-white body-font pt-[10rem]">
+                        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+                            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-start">
+                                <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">The full-service <br />interior design <br />studio
+                                </h1>
+                                <p className="mb-8 leading-relaxed"> Leading the digital transformation of railway With decades of maintenance
                         of way expertise and experience, no one knows the rail like Loram. Today,
                         analytics and intelligence with real-time monitoring and the most
-                        precise application of maintenance of way activities.
-                    </p>
-                    <br></br>
-                    <button className='btn bg-slate-600 text-white font-semibold px-3 py-1 rounded'>
-                        Book Now
-                    </button>
-                </div>
+                        precise application of maintenance of way activities.</p>
+                                <div className="flex justify-center">
+                                    <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Learn more</button>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+            </div>
             </div>
     </div>
   )
