3cfe5e1b386f95925ad137b33b265853de571609
301Fatima <132878418+301Fatima@users.noreply.github.com>
2023-12-28 19:04:59 +0500

    Update Footer.jsx

--- src/component/Navbarlinkscompo/Footer.jsx
+++ src/component/Navbarlinkscompo/Footer.jsx
@@ -73,6 +73,7 @@ const Footer = () => {
     </div>
     <p className="mt-8 text-base leading-6 text-center font-bold text-gray-900">
       © 2021 Rochic, Inc. All rights reserved.
+      <br></br> @developed by Aroosa
     </p>
   </div>
 </section>
